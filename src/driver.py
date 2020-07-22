53f2ef3469cba06777143bdd049699e192d49a49
Kyrylo Maksymenko <saklar13@gmail.com>
2020-07-22 19:09:33 +0300

    add auto release

--- src/driver.py
+++ src/driver.py
@@ -1,139 +1,59 @@
+from cloudshell.shell.core.driver_context import AutoLoadCommandContext, AutoLoadDetails
 from cloudshell.shell.core.driver_utils import GlobalLock
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
 from cloudshell.shell.core.session.logging_session import LoggingSessionContext
 from cloudshell.shell.standards.networking.autoload_model import NetworkingResourceModel
 from cloudshell.shell.standards.networking.driver_interface import (
-    NetworkingResourceDriverInterface
+    NetworkingResourceDriverInterface,
 )
 from cloudshell.shell.standards.networking.resource_config import (
-    NetworkingResourceConfig
+    NetworkingResourceConfig,
 )
 
 
-class DutShell2GDriver (ResourceDriverInterface, NetworkingResourceDriverInterface):
-    SHELL_NAME = 'DutShell2G'
+class DutShell2GDriver(ResourceDriverInterface, NetworkingResourceDriverInterface):
+    SHELL_NAME = "DutShell2G"
 
     def __init__(self):
-        """
-        ctor must be without arguments, it is created with reflection at run time
-        """
         pass
 
     def initialize(self, context):
-        """
-        Initialize the driver session, this function is called everytime a new instance of the driver is created
-        This is a good place to load and cache the driver configuration, initiate sessions etc.
-        :param InitCommandContext context: the context the command runs on
-        """
         pass
 
-    def restore(self, context, cancellation_context, path, configuration_type, restore_method, vrf_management_name):
-        """
-        Restores a configuration file
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str path: The path to the configuration file, including the configuration file name.
-        :param str restore_method: Determines whether the restore should append or override the current configuration.
-        :param str configuration_type: Specify whether the file should update the startup or running config.
-        :param str vrf_management_name: Optional. Virtual routing and Forwarding management name
-        """
+    def restore(
+        self, context, path, configuration_type, restore_method, vrf_management_name,
+    ):
         pass
 
-    def save(self, context, cancellation_context, folder_path, configuration_type, vrf_management_name):
-        """
-        Creates a configuration file and saves it to the provided destination
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str configuration_type: Specify whether the file should update the startup or running config. Value can one
-        :param str folder_path: The path to the folder in which the configuration file will be saved.
-        :param str vrf_management_name: Optional. Virtual routing and Forwarding management name
-        :return The configuration file name.
-        :rtype: str
-        """
+    def save(
+        self, context, folder_path, configuration_type, vrf_management_name,
+    ):
         pass
 
-    def load_firmware(self, context, cancellation_context, path, vrf_management_name):
-        """
-        Upload and updates firmware on the resource
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param str path: path to tftp server where firmware file is stored
-        :param str vrf_management_name: Optional. Virtual routing and Forwarding management name
-        """
+    def load_firmware(self, context, path, vrf_management_name):
         pass
 
-    def run_custom_command(self, context, cancellation_context, custom_command):
-        """
-        Executes a custom command on the device
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str custom_command: The command to run. Note that commands that require a response are not supported.
-        :return: the command result text
-        :rtype: str
-        """
+    def run_custom_command(self, context, custom_command):
         pass
 
-    def run_custom_config_command(self, context, cancellation_context, custom_command):
-        """
-        Executes a custom command on the device in configuration mode
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str custom_command: The command to run. Note that commands that require a response are not supported.
-        :return: the command result text
-        :rtype: str
-        """
+    def run_custom_config_command(self, context, custom_command):
         pass
 
-    def shutdown(self, context, cancellation_context):
-        """
-        Sends a graceful shutdown to the device
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        """
+    def shutdown(self, context):
         pass
 
-    def orchestration_save(self, context, cancellation_context, mode, custom_params):
-        """
-        Saves the Shell state and returns a description of the saved artifacts and information
-        This command is intended for API use only by sandbox orchestration scripts to implement
-        a save and restore workflow
-        :param ResourceCommandContext context: the context object containing resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str mode: Snapshot save mode, can be one of two values 'shallow' (default) or 'deep'
-        :param str custom_params: Set of custom parameters for the save operation
-        :return: SavedResults serialized as JSON
-        :rtype: OrchestrationSaveResult
-        """
+    def orchestration_save(self, context, mode, custom_params):
         pass
 
-    def orchestration_restore(self, context, cancellation_context, saved_artifact_info, custom_params):
-        """
-        Restores a saved artifact previously saved by this Shell driver using the orchestration_save function
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation. Must be enabled in drivermetadata.xml as well
-        :param str saved_artifact_info: A JSON string representing the state to restore including saved artifacts and info
-        :param str custom_params: Set of custom parameters for the restore operation
-        :return: None
-        """
+    def orchestration_restore(self, context, saved_artifact_info, custom_params):
         pass
 
     def ApplyConnectivityChanges(self, context, request):
-        """
-        Configures VLANs on multiple ports or port-channels
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param str request: A JSON object with the list of requested connectivity changes
-        :return: a json object with the list of connectivity changes which were carried out by the switch
-        :rtype: str
-        """
         pass
 
     @GlobalLock.lock
-    def get_inventory(self, context):
-        """
-        Discovers the resource structure and attributes.
-        :param AutoLoadCommandContext context: the context the command runs on
-        :return Attribute and sub-resource information for the Shell resource you can return an AutoLoadDetails object
-        :rtype: AutoLoadDetails
-        """
+    def get_inventory(self, context: AutoLoadCommandContext) -> AutoLoadDetails:
+        """Create a resource structure."""
         with LoggingSessionContext(context):
             resource_config = NetworkingResourceConfig.from_context(
                 self.SHELL_NAME, context
@@ -156,16 +76,8 @@ class DutShell2GDriver (ResourceDriverInterface, NetworkingResourceDriverInterfa
 
         return resource_model.build()
 
-    def health_check(self,cancellation_context):
-        """
-        Checks if the device is up and connectable
-        :return: str: Success or fail message
-        """
+    def health_check(self, cancellation_context):
         pass
 
     def cleanup(self):
-        """
-        Destroy the driver session, this function is called everytime a driver instance is destroyed
-        This is a good place to close any open sessions, finish writing to log files
-        """
         pass
